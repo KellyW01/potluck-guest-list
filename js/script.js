5707c6e2e15ee626a241aaed55c36ef7ea409726
kelly.w <kelly.conant.winn@gmail.com>
2023-06-07 12:18:42 -0400

    Added event listener for assign dishes

--- js/script.js
+++ js/script.js
@@ -12,22 +12,25 @@ const guestCount = document.querySelector(".attendance");
 const guestFull = document.querySelector(".alert");
 const seeYouSat = document.querySelector("footer");
 
+
+
 addGuestButton.addEventListener("click", function () {
-    const guest = guestInput.value;
-    if (guest !== "") {
-      addToList(guest); //pass guest as an argument?
-      clearInput(); //clears guest name from input box after click event
-      updateGuestCount();
-    }
-  });
-  guestInput.addEventListener("keydown", function (e){
-    guest = guestInput.value;
-    if (guest !== "" && e.key === "Enter") {
-      addToList(guest); //pass guest as an argument?
-      clearInput(); //clears guest name from input box after click event
-      updateGuestCount();
-    }
-  });
+  const guest = guestInput.value;
+  if (guest !== "") {
+    addToList(guest); //pass guest as an argument?
+    clearInput(); //clears guest name from input box after click event
+    updateGuestCount();
+  }
+});
+
+guestInput.addEventListener("keydown", function (e) {
+  guest = guestInput.value;
+  if (guest !== "" && e.key === "Enter") {
+    addToList(guest); //pass guest as an argument?
+    clearInput(); //clears guest name from input box after click event
+    updateGuestCount();
+  }
+});
 
 const clearInput = function () { //clears guest name function
   guestInput.value = ""; //changes input to blank
@@ -36,29 +39,70 @@ const clearInput = function () { //clears guest name function
 //refractor code: 
 
 //function just for adding a guest to the list
-const addToList = function (guest){
-    const listItem = document.createElement("li"); //creates a list item, changed let to const now that function is scoped.
-    listItem.innerText = guest; //changes inner text to guest name
-    guestList.append(listItem); // adds guest name to guest list
+const addToList = function (guest) {
+  const listItem = document.createElement("li"); //creates a list item, changed let to const now that function is scoped.
+  listItem.innerText = guest; //changes inner text to guest name
+  guestList.append(listItem); // adds guest name to guest list
 };
 
 //Update guest list number:
-const updateGuestCount = function (){
+const updateGuestCount = function () {
   const guests = document.querySelectorAll(".guest-list li");
   guestCount.innerText = guests.length;
-//prevent list from going over 8 people
-    if (guests.length ===8){
-      //add the hide class to hide button and input areas
-      guestInputLabel.classList.add("hide");
-      addGuestButton.classList.add("hide");
-      guestInput.classList.add("hide");
-      //remove hide class to show list full message
-      guestFull.classList.remove("hide");
-      seeYouSat.classList.revove("hide");
-      }
-      else { //remove the hide class
-      guestInputLabel.classList.remove("hide");
-      addGuestButton.classList.remove("hide");
-      guestInput.classList.remove("hide");
-      }
+  //prevent list from going over 8 people
+  if (guests.length === 8) {
+    //add the hide class to hide button and input areas
+    guestInputLabel.classList.add("hide");
+    addGuestButton.classList.add("hide");
+    guestInput.classList.add("hide");
+    //remove hide class to show list full message
+    guestFull.classList.remove("hide");
+  }
+  // else { //remove the hide class
+  //   guestInputLabel.classList.remove("hide");
+  //   addGuestButton.classList.remove("hide");
+  //   guestInput.classList.remove("hide");
+  // }
 };
+
+const assignButton = document.querySelector(".assign");
+const assignedItems = document.querySelector(".assigned-items");
+
+
+const assignItems = function () {
+  const potluckItems = [
+    "Potato Salad",
+    "Hummus",
+    "Cookies",
+    "Beer",
+    "Wine",
+    "Fruit Tray",
+    "Cheese & Crackers",
+    "Chips & Salsa",
+    "Juice Boxes",
+    "Veggie Tray",
+    "Brownies",
+    "Watermelon",
+    "Green Salad"
+  ];
+  const allGuests = document.querySelectorAll(".guest-list li");
+  
+  for (let guest of allGuests) {
+    let randomPotluckIndex = Math.floor(Math.random() * potluckItems.length);
+    console.log(randomPotluckIndex);//working
+    let randomPotluckItem = potluckItems[randomPotluckIndex];
+    console.log(randomPotluckItem);
+    let listItem = document.createElement("li");
+    listItem.innerText = `${guest.innerText} is bringing ${randomPotluckItem}.`;
+    //(You’re using guest.innerText to access the name inside the li element. If you used guest without innerText, you’d grab the actual list element instead of the text.)
+    assignedItems.append(listItem);
+    potluckItems.splice(randomPotluckIndex, 1);
+  }
+};
+assignButton.addEventListener("click", function () {
+  assignItems();
+  assignButton.disabled = true;
+  seeYouSat.classList.remove("hide"); //show h3 see you saturday
+  guestList.classList.add("hide");
+}
+);
